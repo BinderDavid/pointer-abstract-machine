58a552e0ed92f53cf20b94ac465064a8f61fef0b
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-08-08 16:24:01 +0200

    Add eval loop

--- app/Main.hs
+++ app/Main.hs
@@ -25,4 +25,9 @@ run _ = putStrLn "Unknown argument"
 -- Running an example
 
 runCommand :: MachineState -> IO ()
-runCommand ms = putStrLn (pretty ms)
+runCommand ms = do
+    putStrLn (pretty ms)
+    _ <- getLine
+    let ms' = computeStep ms
+    runCommand ms'
+
