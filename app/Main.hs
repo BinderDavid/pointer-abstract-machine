748f68dea5aa2e2da0b48b5ee248126ac6ae1e98
Ingo Skupin <ingo.skupin@gmail.com>
2022-08-09 16:34:51 +0200

    fix bug in stack lookup function

--- app/Main.hs
+++ app/Main.hs
@@ -18,10 +18,13 @@ ex1 :: Command
 ex1 = Cut idTm CntTop
 
 -- | (\x.x) (\x.x)
--- = mu alpha. < (\x.x) | (\lambda x.x) . alpha
+-- = mu α. < (\x.x) | (\lambda x.x) . α
 ex2 :: Command
-ex2 = Cut (TmMu "alpha" (Cut idTm (CntCallStack idTm (CntVar "alpha")))) CntTop
+ex2 = Cut (TmMu "α" (Cut idTm (CntCallStack idTm (CntVar "α")))) CntTop
 
+-- < μ α. < (λ x. x) | α>  | μ x. <x | ⊤ >
+ex3 :: Command
+ex3 = Cut (TmMu "α" (Cut idTm (CntMu "y" (Cut (TmVar "y") (CntVar "α"))))) (CntMu "x" (Cut (TmVar "x") CntTop))
 
 -- Command line argument logic
 
@@ -33,6 +36,7 @@ main = do
 run :: [String] -> IO ()
 run ["ex1"] = putStrLn ("\nComputing command " <> pretty ex1 <> "\n") >> runCommand (embedCommand ex1)
 run ["ex2"] = putStrLn ("\nComputing command " <> pretty ex2 <> "\n") >> runCommand (embedCommand ex2)
+run ["ex3"] = putStrLn ("\nComputing command " <> pretty ex3 <> "\n") >> runCommand (embedCommand ex3)
 run _ = putStrLn "Unknown argument"
 
 -- Running an example
