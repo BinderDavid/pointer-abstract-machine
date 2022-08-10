80c9f7fe6c1c4d16849300904508c916e4ee7176
Ingo Skupin <ingo.skupin@gmail.com>
2022-08-10 13:22:04 +0200

    add some examples

--- app/Main.hs
+++ app/Main.hs
@@ -22,11 +22,41 @@ ex1 = Cut idTm CntTop
 ex2 :: Command
 ex2 = Cut (TmMu "α" (Cut idTm (CntCallStack idTm (CntVar "α")))) CntTop
 
--- < μ α. < (λ x. x) | α>  | μ x. <x | ⊤ >
+-- < μ α.< λx.x | ~μ y.< y | α > > | ~μ x.< x | □ > >
 ex3 :: Command
 ex3 = Cut (TmMu "α" (Cut idTm (CntMu "y" (Cut (TmVar "y") (CntVar "α"))))) (CntMu "x" (Cut (TmVar "x") CntTop))
 
+-- < μ α.< λx.x | ~μ y.< y | ~μ z.< z | ~μ w.< w | α > > > > | ~μ x.< x | □ > >
+ex4 :: Command
+ex4 = Cut (TmMu "α" (Cut idTm (CntMu "y" (Cut (TmVar "y")
+                                (CntMu "z" (Cut (TmVar "z")
+                                  (CntMu "w" (Cut (TmVar "w") (CntVar "α")))))))))
+          (CntMu "x" (Cut (TmVar "x") CntTop))
+
+-- < μ α.< λx.x | ~μ x.< x | ~μ x.< x | ~μ x.< x | α > > > > | ~μ x.< x | □ > >
+ex5 :: Command
+ex5 = Cut (TmMu "α" (Cut idTm (CntMu "x" (Cut (TmVar "x")
+                                (CntMu "x" (Cut (TmVar "x")
+                                  (CntMu "x" (Cut (TmVar "x") (CntVar "α")))))))))
+          (CntMu "x" (Cut (TmVar "x") CntTop))
+
+-- < μ α.< λx.x | ~μ y.< y | ~μ z.< z | ~μ w.< y | α > > > > | ~μ x.< x | □ > >
+ex6 :: Command
+ex6 = Cut (TmMu "α" (Cut idTm (CntMu "y" (Cut (TmVar "y")
+                                (CntMu "z" (Cut (TmVar "z")
+                                  (CntMu "w" (Cut (TmVar "y") (CntVar "α")))))))))
+          (CntMu "x" (Cut (TmVar "x") CntTop))
 -- Command line argument logic
+
+examples :: [(String,Command)]
+examples =  [ ("ex1", ex1)
+            , ("ex2", ex2)
+            , ("ex3", ex3)
+            , ("ex4", ex4)
+            , ("ex5", ex5)
+            , ("ex6", ex6)
+            ]
+              
 
 main :: IO ()
 main = do
@@ -34,9 +64,9 @@ main = do
     run args
 
 run :: [String] -> IO ()
-run ["ex1"] = putStrLn ("\nComputing command " <> pretty ex1 <> "\n") >> runCommand (embedCommand ex1)
-run ["ex2"] = putStrLn ("\nComputing command " <> pretty ex2 <> "\n") >> runCommand (embedCommand ex2)
-run ["ex3"] = putStrLn ("\nComputing command " <> pretty ex3 <> "\n") >> runCommand (embedCommand ex3)
+run [s] = case lookup s examples of
+            Nothing -> putStrLn "Unknown argument"
+            Just ex ->  putStrLn ("\nComputing command " <> pretty ex <> "\n") >> runCommand (embedCommand ex)
 run _ = putStrLn "Unknown argument"
 
 -- Running an example
