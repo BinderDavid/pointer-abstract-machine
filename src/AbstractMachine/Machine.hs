ed0a8a3db65752a4594560f0978cc412b19f6b0c
BinderDavid <binderd@informatik.uni-tuebingen.de>
2022-08-25 17:50:45 +0200

    Implement CBV/CBN semantics

--- src/AbstractMachine/Machine.hs
+++ src/AbstractMachine/Machine.hs
@@ -35,7 +35,8 @@ computeStep :: MachineState -> Either String (MachineState, ComputeStep)
 -- Popping the top of the stack.
 -------------------------------------------------------------------------------
 computeStep (MkMachineState hd@(CutState _ _ pt1 _ pt2) stack) | max pt1 pt2 + 1 < topOfStack stack = do
-    Right (MkMachineState hd (restrictStack stack (max pt1 pt2)), GarbageCollectionStep)
+    let newStack = restrictStack stack (max pt1 pt2)
+    pure (MkMachineState hd newStack, GarbageCollectionStep)
 -------------------------------------------------------------------------------
 -- Logical Steps
 -------------------------------------------------------------------------------
@@ -48,18 +49,33 @@ computeStep (MkMachineState (CutState _ (TmLambda x funbody) _p1 (CntCallStack f
 -------------------------------------------------------------------------------
 -- Mu/Tilde mu steps
 -------------------------------------------------------------------------------
--- Reducing mu abstractions.
+-- We have a mu / tilde mu redex, which we have to disambiguate using the eval
+-- order.
+computeStep (MkMachineState (CutState eo prd@(TmMu x (Cut l1 l2)) p1 cns@(CntMu y (Cut r1 r2)) p2) stack) = do
+    let tos = topOfStack stack
+    case eo of
+        CBV -> do
+            let newHead = CutState CBV l1 tos l2 tos
+            let newStack = extendStack stack x (ContinuationBinding cns p2)
+            let newState = MkMachineState newHead newStack
+            pure (newState, MuStep)
+        CBN -> do
+            let newHead = CutState CBV r1 tos r2 tos
+            let newStack = extendStack stack y (TermBinding prd p1)
+            let newState = MkMachineState newHead newStack
+            pure (newState, MuTildeStep)
+-- Reducing mu abstractions. (Continuation is not tilde-mu)
 computeStep (MkMachineState (CutState _ (TmMu x (Cut tm cont)) _p1 cont' p2) stack) = do
     let newHead = CutState CBV tm (topOfStack stack) cont (topOfStack stack)
     let newStack = extendStack stack x (ContinuationBinding cont' p2)
     let newState = MkMachineState newHead newStack
-    Right (newState, MuStep)
--- Reducing tilde mu abstractions
-computeStep (MkMachineState (CutState _ tm' p1 (CntMu x (Cut tm cnt)) _p2) stack) = do
+    pure (newState, MuStep)
+-- Reducing tilde mu abstractions (Producer is not mu)
+computeStep (MkMachineState (CutState _ tm' p1 (CntMu y (Cut tm cnt)) _p2) stack) = do
     let newHead = CutState CBV tm (topOfStack stack) cnt (topOfStack stack)
-    let newStack = extendStack stack x (TermBinding tm' p1)
+    let newStack = extendStack stack y (TermBinding tm' p1)
     let newState = MkMachineState newHead newStack
-    Right (newState, MuTildeStep)
+    pure (newState, MuTildeStep)
 -------------------------------------------------------------------------------
 -- Looking up Variables
 -------------------------------------------------------------------------------
